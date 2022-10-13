628bc5e81c5e119bacafffdd2418de92a3a2bdf5
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-14 00:19:31 +0100

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,8 @@
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = (
     () => {
