8ec5249dc796b1ed85a9fe99cc26c5a323b8320b
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-13 12:31:47 +0100

    Update mint.tsx

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -2,6 +2,14 @@ import { useAddress, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
+import {
+  useClaimedNFTSupply,
+  useContractMetadata,
+  useUnclaimedNFTSupply,
+  useActiveClaimCondition,
+  Web3Button,
+  useContract,
+}
 
 const Mint: NextPage = () => {
   const router = useRouter();
@@ -10,11 +18,43 @@ const Mint: NextPage = () => {
 
   // Function to connect to the user's Metamask wallet
   const connectWithMetamask = useMetamask();
-
+const myNftDropContractAddress = "0x0cBF1C44bc8591fc811Ad24aB27603581796800A";
   // Get the NFT Collection contract
   const nftDropContract = useNFTDrop(
     "0x0cBF1C44bc8591fc811Ad24aB27603581796800A"
   );
+  const { contract: nftDrop } = useContract(myNftDropContractAddress);
+  
+    // The amount the user claims
+  const [quantity, setQuantity] = useState(1); // default to 1
+
+  // Load contract metadata
+  const { data: contractMetadata } = useContractMetadata(nftDrop);
+
+  // Load claimed supply and unclaimed supply
+  const { data: unclaimedSupply } = useUnclaimedNFTSupply(nftDrop);
+  const { data: claimedSupply } = useClaimedNFTSupply(nftDrop);
+
+  // Load the active claim condition
+  const { data: activeClaimCondition } = useActiveClaimCondition(nftDrop);
+
+  // Check if there's NFTs left on the active claim phase
+  const isNotReady =
+    activeClaimCondition &&
+    parseInt(activeClaimCondition?.availableSupply) === 0;
+
+  // Check if there's any NFTs left
+  const isSoldOut = unclaimedSupply?.toNumber() === 0;
+
+  // Check price
+  const price = parseUnits(
+    activeClaimCondition?.currencyMetadata.displayValue || "0",
+    activeClaimCondition?.currencyMetadata.decimals
+  );
+
+  // Multiply depending on quantity
+  const priceToMint = price.mul(quantity);
+  
 
   async function claimNft() {
     try {
