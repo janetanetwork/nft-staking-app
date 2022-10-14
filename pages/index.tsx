38d4007a58de9581f8776f5e69b9f3828e38ae29
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-14 08:46:32 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ const Home: NextPage = () => {
   return (
     <div>
        <div>
-          <ul>
-            <li className="inline-block ml-5 mt-2 text-white bg-black opacity-20 pl-5 pr-5 pb-2 pt-2 rounded-xl">
+          <ul className={styles.roll} >
+            <li>
               <Link href="/"><a>home</a></Link>
             </li>
-            <li className="inline-block ml-2 mt-2 text-white bg-black opacity-20 pl-5 pr-5 pb-2 pt-2 rounded-xl">
+            <li>
               <Link href="/mint"><a>Mint</a></Link>
             </li>
-            <li className="inline-block ml-2 mt-2 text-white bg-black opacity-20 pl-5 pr-5 pb-2 pt-2 rounded-xl">
+            <li>
               <Link href="/stake"><a>Stake</a></Link>
             </li>
           </ul>
