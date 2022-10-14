54bf1c7356019a230132f8e64f63e5e8240d1172
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-14 08:09:31 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
