53dbe8e1158a28df69b6627a0f446d08057024f6
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-14 12:51:54 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
        
           <img src={`/welcome.png`} alt="drop" width="100%" height="100%" />
         <p className={styles.textm}>
-          10,000 unique collectible characters with proof of ownership stored on the Ethereum blockchain. The project that inspired the modern CryptoArt movement.
+          10,000 unique collectible characters with proof of ownership stored on the Polygon blockchain. The project that inspired the modern CryptoArt movement.
         </p>
         
       
@@ -60,6 +60,12 @@ const Home: NextPage = () => {
               The PixieVerse NFTs are 10,000 uniquely generated characters. No two are exactly alike, and each one of them can be officially owned by a single person on the Polygon blockchain,
 Each NFT can be staked to earn the ERC20 PIXIECOIN which will be used as the official currency for the Pixie Ecosystem, which will consist of numerous beneficial DAPPS. .
             </p>
+          <button
+          className={`${styles.mainButton} ${styles.spacerBottom}`}
+          
+        >
+         Mint NFT
+        </button>
         </div>
 
         <div
@@ -73,6 +79,12 @@ Each NFT can be staked to earn the ERC20 PIXIECOIN which will be used as the off
           <p className={styles.selectBoxDescription}>
             You can stake your NFTS to earn PIXIECOIN, the official token of the pixieverse ecosystem, the token will be used to vote on our DAO, use as LP token on our DEX(comming soon!) all also it will be used as the offical currency for our play to earn games and dapps.
           </p>
+          <button
+          className={`${styles.mainButton} ${styles.spacerBottom}`}
+          
+        >
+          Stake NFT
+        </button>
         </div>
       </div>
     </div>
