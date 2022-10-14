f6963ed1a9adf5b69b2b662cf14bc6ac32c6cac7
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-14 15:59:11 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,12 +42,12 @@ const Home: NextPage = () => {
     
       <img src={`/gg.gif`} alt="drop" width="100%" height="50%" className={styles.radius} />
        
-          <img src={`/welcome.png`} alt="drop" width="100%" height="100%" />
+          <img src={`/welcomen.png`} alt="drop" width="100%" height="100%" />
         <p className={styles.textm}>
           10,000 unique collectible characters with proof of ownership stored on the Polygon blockchain. The project that inspired the modern CryptoArt movement.
         </p>
         <img src={`/road.png`} alt="drop" width="100%" height="100%" />
-      
+        <img src={`/rop.png`} alt="drop" width="100%" height="100%" />
     
       {/* Top Section */}
       
@@ -59,7 +59,7 @@ const Home: NextPage = () => {
         >
           {/* Mint a new NFT */}
             <img src={`/icons/drop.webp`} alt="drop" />
-            <h2 className={styles.selectBoxTitle}>Mint a new NFT</h2>
+            <img src={`/mint.png`} alt="drop" width="100%" height="100%" />
             <p className={styles.selectBoxDescription}>
               The PixieVerse NFTs are 10,000 uniquely generated characters. No two are exactly alike, and each one of them can be officially owned by a single person on the Polygon blockchain,
 Each NFT can be staked to earn the ERC20 PIXIECOIN which will be used as the official currency for the Pixie Ecosystem, which will consist of numerous beneficial DAPPS. .
@@ -79,7 +79,7 @@ Each NFT can be staked to earn the ERC20 PIXIECOIN which will be used as the off
         >
           {/* Staking an NFT */}
           <img src={`/icons/token.webp`} alt="drop" />
-          <h2 className={styles.selectBoxTitle}>Stake Your NFTs</h2>
+          <img src={`/stake.png`} alt="drop" width="100%" height="100%" />
           <p className={styles.selectBoxDescription}>
             You can stake your NFTS to earn PIXIECOIN, the official token of the pixieverse ecosystem, the token will be used to vote on our DAO, use as LP token on our DEX(comming soon!) all also it will be used as the offical currency for our play to earn games and dapps.
           </p>
