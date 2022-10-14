3a6cefc02034c7d0e8b10cbab1ab0a154574cd2b
janetanetwork <112816089+janetanetwork@users.noreply.github.com>
2022-10-14 12:32:40 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,8 +37,8 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <section>
       <img src={`/gg.gif`} alt="drop" width="50%" height="50%" />
-      <div className={styles.text}>
-        <img src={`/welcome.png`} alt="drop" width="50%" height="50%" />
+        <p></p>
+        <img src={`/welcome.png`} alt="drop" width="100%" height="50%" />
         <p className={styles.textm}>
           10,000 unique collectible characters with proof of ownership stored on the Ethereum blockchain. The project that inspired the modern CryptoArt movement.
         </p>
